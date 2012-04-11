1d68ce278eda578622af0cd6abd4804568d5cfd3
Michał Czyż <michalczyz@gmail.com>
2012-04-11 16:52:07 +0300

    Update lib/zendesk/client/search.rb

--- lib/zendesk/client/search.rb
+++ lib/zendesk/client/search.rb
@@ -1,6 +1,15 @@
 module Zendesk
   class Client
     module Search
+      def search(*args)
+        SearchTicketsCollection.new(self, *args)
+      end
+    end
+
+    class SearchTicketsCollection < Collection
+      def initialize(client, *args)
+        super(client, :search, *args)
+      end
     end
   end
 end
