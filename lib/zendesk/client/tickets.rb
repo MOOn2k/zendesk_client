286933918c6934d845cc57b52ccda7c62f8f2594
Szymon Kieloch <skieloch@gmail.com>
2012-04-12 21:18:59 +0200

    Add API implementation for creating a ticket with attachments.

--- lib/zendesk/client/tickets.rb
+++ lib/zendesk/client/tickets.rb
@@ -19,8 +19,23 @@ module Zendesk
         self
       end
 
+      def create(data={})
+        create_files(data) if data[:attachments]
+        super(data)
+      end
+
       # TODO: @zendesk.ticket(123).public_comment({ ... })
       # TODO: @zendesk.ticket(123).private_comment({ ... })
+
+      private
+
+      def create_files(data)
+        token = nil
+        data.delete(:attachments).each do |name|
+          token = @client.attachments.create(:filename => name, :token => token)[:token]
+        end
+        data[:uploads] = token
+      end
     end
   end
 end
